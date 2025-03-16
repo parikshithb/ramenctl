fb0b8c33e526204add5584d670c3e12e3e904877
Nir Soffer <nsoffer@redhat.com>
2025-03-16 20:22:16 +0200

    test: Run a single real test with the env
    Create a test.Test instance from the first test in config.Tests, and run
    a complete flow similar to ramen/e2e/dr_test flow.
    
    Running all tests in parallel will be implemented later.
    
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>

--- pkg/test/run.go
+++ pkg/test/run.go
@@ -3,60 +3,38 @@
 
 package test
 
-import (
-	"fmt"
-	"time"
-
-	"github.com/ramendr/ramenctl/pkg/config"
-	"github.com/ramendr/ramenctl/pkg/console"
-)
-
 func Run(configFile string, outputDir string) error {
-	config, err := config.ReadConfig(configFile)
+	cmd, err := newCommand("test-run", configFile, outputDir)
 	if err != nil {
 		return err
 	}
 
 	// We want to run all tests in parallel, but for now lets run one test.
-	test := config.Tests[0]
+	test := newTest(cmd.Config.Tests[0], cmd)
 
-	app := fmt.Sprintf("test-%s-%s-%v", test.Deployer, test.Workload, test.PVCSpec)
-	primary := config.Clusters["c1"].Name
-	secondary := config.Clusters["c2"].Name
+	if err := test.Deploy(); err != nil {
+		return err
+	}
 
-	console.Info("Using report %q", outputDir)
+	if err := test.Protect(); err != nil {
+		return err
+	}
 
-	console.Progress("Deploying application %q on cluster %q", app, primary)
-	time.Sleep(2 * time.Second)
-	console.Completed("Application %q deployed on cluster %q", app, primary)
+	if err := test.Failover(); err != nil {
+		return err
+	}
 
-	console.Progress("Protecting application %q in cluster %q", app, primary)
-	time.Sleep(5 * time.Second)
-	console.Completed("Application %q protected in cluster %q", app, primary)
+	if err := test.Relocate(); err != nil {
+		return err
+	}
 
-	console.Progress("Failing over application %q to cluster %q", app, secondary)
-	time.Sleep(10 * time.Second)
-	console.Completed("Application %q failed over to cluster %q", app, secondary)
+	if err := test.Unprotect(); err != nil {
+		return err
+	}
 
-	console.Progress("Unprotecting application %q in cluster %q", app, secondary)
-	time.Sleep(5 * time.Second)
-	console.Completed("Application %q unprotected in cluster %q", app, secondary)
-
-	console.Progress("Protecting application %q in cluster %q", app, secondary)
-	time.Sleep(5 * time.Second)
-	console.Completed("Application %q protected in cluster %q", app, secondary)
-
-	console.Progress("Relocating application %q to cluster %q", app, primary)
-	time.Sleep(10 * time.Second)
-	console.Completed("Application %q relocated to cluster %q", app, primary)
-
-	console.Progress("Unprotecting application %q in cluster %q", app, primary)
-	time.Sleep(5 * time.Second)
-	console.Completed("Application %q unprotected in cluster %q", app, primary)
-
-	console.Progress("Undeploying application %q from cluster %q", app, primary)
-	time.Sleep(2 * time.Second)
-	console.Completed("Application %q undeployed from cluster %q", app, primary)
+	if err := test.Undeploy(); err != nil {
+		return err
+	}
 
 	return nil
 }
