5a85c91f381a3e5bda289797a174681c6515b7ea
Nir Soffer <nsoffer@redhat.com>
2025-03-16 18:42:43 +0200

    test: Implement Context
    The test.Context implements ramen/e2e/test.Context. This is the way to
    bind all the details needed for a single test. We implement only the
    test.Context interface so we can use later e2e implementation, and keep
    the code simple as possible. The ramenctl specific parts will be
    implemented on top of the context.
    
    Unlike ramen/e2e/test.Context, we keep a Command instead of the env and
    config. This matches the actual model - all tests share the same config
    and env, which are read-only at the point we use them.
    
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>

---
+++ pkg/test/context.go
@@ -0,0 +1,69 @@
+// SPDX-FileCopyrightText: The RamenDR authors
+// SPDX-License-Identifier: Apache-2.0
+
+package test
+
+import (
+	"fmt"
+
+	"go.uber.org/zap"
+
+	"github.com/ramendr/ramen/e2e/types"
+)
+
+// Context implements types.Context interface.
+type Context struct {
+	workload types.Workload
+	deployer types.Deployer
+	name     string
+	logger   *zap.SugaredLogger
+	cmd      *Command
+}
+
+var _ types.Context = &Context{}
+
+func newContext(workload types.Workload, deployer types.Deployer, cmd *Command) *Context {
+	name := fmt.Sprintf("%s-%s", deployer.GetName(), workload.GetName())
+	return &Context{
+		workload: workload,
+		deployer: deployer,
+		name:     name,
+		logger:   cmd.Logger.Named(name),
+		cmd:      cmd,
+	}
+}
+
+func (c *Context) Deployer() types.Deployer {
+	return c.deployer
+}
+
+func (c *Context) Workload() types.Workload {
+	return c.workload
+}
+
+func (c *Context) Name() string {
+	return c.name
+}
+
+func (c *Context) ManagementNamespace() string {
+	if ns := c.deployer.GetNamespace(c); ns != "" {
+		return ns
+	}
+	return c.AppNamespace()
+}
+
+func (c *Context) AppNamespace() string {
+	return c.cmd.NamespacePrefix + c.name
+}
+
+func (c *Context) Logger() *zap.SugaredLogger {
+	return c.logger
+}
+
+func (c *Context) Env() *types.Env {
+	return c.cmd.Env
+}
+
+func (c *Context) Config() *types.Config {
+	return c.cmd.Config
+}
