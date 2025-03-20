299c121b942d746024830afd556bd6d75048eb85
Nir Soffer <nsoffer@redhat.com>
2025-03-20 21:51:36 +0200

    Keep test status and configuration
    Add Config and Status fields to Test. When a test fails, we mark the
    test as failed. The Config and Status will be used to report the test
    result.
    
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>

--- pkg/test/test.go
+++ pkg/test/test.go
@@ -17,6 +17,8 @@ import (
 // Test perform DR opetaions for testing DR flow.
 type Test struct {
 	types.Context
+	Config types.TestConfig
+	Status Status
 }
 
 // newTest creates a test from test configuration and command context.
@@ -36,14 +38,18 @@ func newTest(tc types.TestConfig, cmd *Command) *Test {
 		panic(err)
 	}
 
-	return &Test{Context: newContext(workload, deployer, cmd)}
+	return &Test{
+		Context: newContext(workload, deployer, cmd),
+		Config:  tc,
+		Status:  Passed,
+	}
 }
 
 func (t *Test) Deploy() error {
 	console.Progress("Deploy application %q", t.Name())
 	if err := t.Deployer().Deploy(t.Context); err != nil {
 		err := fmt.Errorf("failed to deploy application %q: %w", t.Name(), err)
-		t.Logger().Error(err)
+		t.Fail(err)
 		return err
 	}
 	console.Completed("Application %q deployed", t.Name())
@@ -54,7 +60,7 @@ func (t *Test) Undeploy() error {
 	console.Progress("Undeploy application %q", t.Name())
 	if err := t.Deployer().Undeploy(t.Context); err != nil {
 		err := fmt.Errorf("failed to undeploy application %q: %w", t.Name(), err)
-		t.Logger().Error(err)
+		t.Fail(err)
 		return err
 	}
 	console.Completed("Application %q undeployed", t.Name())
@@ -65,7 +71,7 @@ func (t *Test) Protect() error {
 	console.Progress("Protect application %q", t.Name())
 	if err := dractions.EnableProtection(t.Context); err != nil {
 		err := fmt.Errorf("failed to protect application %q: %w", t.Name(), err)
-		t.Logger().Error(err)
+		t.Fail(err)
 		return err
 	}
 	console.Completed("Application %q protected", t.Name())
@@ -76,7 +82,7 @@ func (t *Test) Unprotect() error {
 	console.Progress("Unprotect application %q", t.Name())
 	if err := dractions.DisableProtection(t.Context); err != nil {
 		err := fmt.Errorf("failed to unprotect application %q: %w", t.Name(), err)
-		t.Logger().Error(err)
+		t.Fail(err)
 		return err
 	}
 	console.Completed("Application %q unprotected", t.Name())
@@ -87,7 +93,7 @@ func (t *Test) Failover() error {
 	console.Progress("Failover application %q", t.Name())
 	if err := dractions.Failover(t.Context); err != nil {
 		err := fmt.Errorf("failed to failover application %q: %w", t.Name(), err)
-		t.Logger().Error(err)
+		t.Fail(err)
 		return err
 	}
 	console.Completed("Application %q failed over", t.Name())
@@ -98,9 +104,14 @@ func (t *Test) Relocate() error {
 	console.Progress("Relocate application %q", t.Name())
 	if err := dractions.Relocate(t.Context); err != nil {
 		err := fmt.Errorf("failed to relocate application %q: %w", t.Name(), err)
-		t.Logger().Error(err)
+		t.Fail(err)
 		return err
 	}
 	console.Completed("Application %q relocated", t.Name())
 	return nil
+}
+
+func (t *Test) Fail(err error) {
+	t.Logger().Error(err)
+	t.Status = Failed
 }
