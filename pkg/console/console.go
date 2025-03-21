d663348d1bc15344eaa6694a591f30607a2bc654
Nir Soffer <nsoffer@redhat.com>
2025-03-21 21:06:18 +0200

    console: Adapt for concurrent tests
    When running tests in parallel we will have multiple tests in progress,
    so we cannot use the `\r` to replace the progress message with a
    completion message. Simplify the code so we log a complete line for all
    messages.
    
    Also replace the progress emoji to 🔎 which look little better.
    
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>

--- pkg/console/console.go
+++ pkg/console/console.go
@@ -6,24 +6,18 @@ package console
 import (
 	"fmt"
 	"os"
-	"strings"
 )
-
-// TODO: Measure actual console and update when window size changes
-var width = 80
 
 func Info(format string, args ...any) {
 	fmt.Printf("⭐ "+format+"\n", args...)
 }
 
 func Progress(format string, args ...any) {
-	line := fmt.Sprintf("👀 "+format+" ...", args...)
-	fmt.Print(pad(line), "\r")
+	fmt.Printf("🔎 "+format+" ...\n", args...)
 }
 
 func Completed(format string, args ...any) {
-	line := fmt.Sprintf("✅ "+format, args...)
-	fmt.Print(pad(line), "\n")
+	fmt.Printf("✅ "+format+"\n", args...)
 }
 
 func Error(err error) {
@@ -33,12 +27,4 @@ func Error(err error) {
 func Fatal(err error) {
 	Error(err)
 	os.Exit(1)
-}
-
-// pad string to console width, leaving room for the line terminator.
-func pad(s string) string {
-	if len(s) < width-1 {
-		return s + strings.Repeat(" ", width-len(s)-1)
-	}
-	return s
 }
