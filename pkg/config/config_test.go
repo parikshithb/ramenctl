28fca36f74f9e995fb6ff097f4f816ebf3a14ddf
Nir Soffer <nsoffer@redhat.com>
2025-04-07 17:36:13 +0300

    init: Optimize storage classes for ODF
    When running as `odf dr` use the default ODF storage class names that
    are more likely to work instead of drenv storage class name, that will
    never work. This minimizes manual configuration, making `odf dr` easier
    to use.
    
    When using the --envfile flag we use default drenv storage classes since
    envfile is never needed for ODF cluster. Currently the envfile does not
    include the storage class names so we must hard code them in ramenctl.
    This makes it possible to use `odf dr init` with a drenv environment,
    which can help developer that work mostly with an ODF cluster.
    
    We can consider later smarter way to configure the storage classes like
    listing the available storage classes and let the user choose.
    
    Fixes: #83
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -32,10 +32,27 @@ func TestReadEnvFile(t *testing.T) {
 func TestSample(t *testing.T) {
 	sample := config.NewSample("ramenctl")
 	expected := &config.Sample{
-		CommandName:         "ramenctl",
-		HubKubeconfig:       "hub/config",
-		PrimaryKubeconfig:   "primary/config",
-		SecondaryKubeconfig: "secondary/config",
+		CommandName:            "ramenctl",
+		HubKubeconfig:          "hub/config",
+		PrimaryKubeconfig:      "primary/config",
+		SecondaryKubeconfig:    "secondary/config",
+		RBDStorageClassName:    "rook-ceph-block",
+		CephFSStorageClassName: "rook-cephfs-fs1",
+	}
+	if !reflect.DeepEqual(expected, sample) {
+		t.Fatalf("expected %+v, got %+v", expected, sample)
+	}
+}
+
+func TestSampleForODF(t *testing.T) {
+	sample := config.NewSample("odf dr")
+	expected := &config.Sample{
+		CommandName:            "odf dr",
+		HubKubeconfig:          "hub/config",
+		PrimaryKubeconfig:      "primary/config",
+		SecondaryKubeconfig:    "secondary/config",
+		RBDStorageClassName:    "ocs-storagecluster-ceph-rbd",
+		CephFSStorageClassName: "ocs-storagecluster-cephfs",
 	}
 	if !reflect.DeepEqual(expected, sample) {
 		t.Fatalf("expected %+v, got %+v", expected, sample)
@@ -52,10 +69,12 @@ func TestSampleFromEnv(t *testing.T) {
 	}
 	sample := config.SampleFromEnv("ramenctl", env)
 	expected := &config.Sample{
-		CommandName:         "ramenctl",
-		HubKubeconfig:       env.KubeconfigPath("hub"),
-		PrimaryKubeconfig:   env.KubeconfigPath("dr1"),
-		SecondaryKubeconfig: env.KubeconfigPath("dr2"),
+		CommandName:            "ramenctl",
+		HubKubeconfig:          env.KubeconfigPath("hub"),
+		PrimaryKubeconfig:      env.KubeconfigPath("dr1"),
+		SecondaryKubeconfig:    env.KubeconfigPath("dr2"),
+		RBDStorageClassName:    "rook-ceph-block",
+		CephFSStorageClassName: "rook-cephfs-fs1",
 	}
 	if !reflect.DeepEqual(expected, sample) {
 		t.Fatalf("expected %+v, got %+v", expected, sample)
--- pkg/config/sample.go
+++ pkg/config/sample.go
@@ -13,27 +13,46 @@ import (
 var sampleConfig string
 
 type Sample struct {
-	CommandName         string
-	HubKubeconfig       string
-	PrimaryKubeconfig   string
-	SecondaryKubeconfig string
+	CommandName            string
+	HubKubeconfig          string
+	PrimaryKubeconfig      string
+	SecondaryKubeconfig    string
+	RBDStorageClassName    string
+	CephFSStorageClassName string
 }
 
 func NewSample(commandName string) *Sample {
-	return &Sample{
+	sample := &Sample{
 		CommandName:         commandName,
 		HubKubeconfig:       "hub/config",
 		PrimaryKubeconfig:   "primary/config",
 		SecondaryKubeconfig: "secondary/config",
 	}
+
+	// When running as `odf dr init` we optimize for ODF cluster.
+	// TODO: look up available storage classes in the cluster and let the user choose?
+	if commandName == "odf dr" {
+		sample.RBDStorageClassName = "ocs-storagecluster-ceph-rbd"
+		sample.CephFSStorageClassName = "ocs-storagecluster-cephfs"
+	} else {
+		sample.RBDStorageClassName = "rook-ceph-block"
+		sample.CephFSStorageClassName = "rook-cephfs-fs1"
+	}
+
+	return sample
 }
 
 func SampleFromEnv(commandName string, env *EnvFile) *Sample {
+	// Using drenv envfile: use drenv storage classes.
 	return &Sample{
 		CommandName:         commandName,
 		HubKubeconfig:       env.KubeconfigPath(env.Ramen.Hub),
 		PrimaryKubeconfig:   env.KubeconfigPath(env.Ramen.Clusters[0]),
 		SecondaryKubeconfig: env.KubeconfigPath(env.Ramen.Clusters[1]),
+
+		// TODO: Get the info from the envfile instead of hard-coding.
+		RBDStorageClassName:    "rook-ceph-block",
+		CephFSStorageClassName: "rook-cephfs-fs1",
 	}
 }
 
