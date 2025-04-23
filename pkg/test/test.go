8ec0a63f89438abe3129fb837a697aed2df67702
Nir Soffer <nsoffer@redhat.com>
2025-04-23 20:13:58 +0300

    test: Simplify step handling and logging
    Add Test.start() helper creating a step and adding it to the steps list.
    This can make it easier to debug since the state of the tests is
    reflected in the steps list.
    
    Since we know the current step, we don't need to pass the step the
    fail() and pass().
    
    Add logs when test is started and passed, so we have the complete flow
    of a test in ramenctl level.
    
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>

--- pkg/test/test.go
+++ pkg/test/test.go
@@ -47,70 +47,77 @@ func newTest(tc types.TestConfig, cmd *Command) *Test {
 }
 
 func (t *Test) Deploy() bool {
-	step := &Step{Name: "deploy"}
+	t.start("deploy")
 	if err := t.Deployer().Deploy(t.Context); err != nil {
-		return t.fail(step, err)
+		return t.fail(err)
 	}
 	console.Pass("Application %q deployed", t.Name())
-	return t.pass(step)
+	return t.pass()
 }
 
 func (t *Test) Undeploy() bool {
-	step := &Step{Name: "undeploy"}
+	t.start("undeploy")
 	if err := t.Deployer().Undeploy(t.Context); err != nil {
-		return t.fail(step, err)
+		return t.fail(err)
 	}
 	console.Pass("Application %q undeployed", t.Name())
-	return t.pass(step)
+	return t.pass()
 }
 
 func (t *Test) Protect() bool {
-	step := &Step{Name: "protect"}
+	t.start("protect")
 	if err := dractions.EnableProtection(t.Context); err != nil {
-		return t.fail(step, err)
+		return t.fail(err)
 	}
 	console.Pass("Application %q protected", t.Name())
-	return t.pass(step)
+	return t.pass()
 }
 
 func (t *Test) Unprotect() bool {
-	step := &Step{Name: "unprotect"}
+	t.start("unprotect")
 	if err := dractions.DisableProtection(t.Context); err != nil {
-		return t.fail(step, err)
+		return t.fail(err)
 	}
 	console.Pass("Application %q unprotected", t.Name())
-	return t.pass(step)
+	return t.pass()
 }
 
 func (t *Test) Failover() bool {
-	step := &Step{Name: "failover"}
+	t.start("failover")
 	if err := dractions.Failover(t.Context); err != nil {
-		return t.fail(step, err)
+		return t.fail(err)
 	}
 	console.Pass("Application %q failed over", t.Name())
-	return t.pass(step)
+	return t.pass()
 }
 
 func (t *Test) Relocate() bool {
-	step := &Step{Name: "relocate"}
+	t.start("relocate")
 	if err := dractions.Relocate(t.Context); err != nil {
-		return t.fail(step, err)
+		return t.fail(err)
 	}
 	console.Pass("Application %q relocated", t.Name())
-	return t.pass(step)
+	return t.pass()
 }
 
-func (t *Test) fail(step *Step, err error) bool {
-	step.Status = Failed
+func (t *Test) start(name string) {
+	step := &Step{Name: name}
 	t.Steps = append(t.Steps, step)
+	t.Logger().Infof("Step %q started", step.Name)
+}
+
+func (t *Test) fail(err error) bool {
+	step := t.Steps[len(t.Steps)-1]
+	step.Status = Failed
 	t.Status = Failed
 	t.Logger().Errorf("Step %q failed: %s", step.Name, err)
 	console.Error("Failed to %s application %q", step.Name, t.Name())
 	return false
 }
 
-func (t *Test) pass(step *Step) bool {
+func (t *Test) pass() bool {
+	step := t.Steps[len(t.Steps)-1]
 	step.Status = Passed
-	t.Steps = append(t.Steps, step)
+	t.Logger().Infof("Step %q passed", step.Name)
 	return true
 }
