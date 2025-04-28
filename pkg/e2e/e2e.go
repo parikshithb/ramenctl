d7d575fe10a6735dc00749965eeea52cc090e0e0
Nir Soffer <nsoffer@redhat.com>
2025-04-28 20:42:11 +0300

    e22: Introduce the e2e package
    The e2e package provides:
    - Testing interface for ramenctl commands
    - Backend implementing the testing interface
    
    We will use the backend for accessing the e2e packages. This will make
    it possible to run fast tests with a mock backend that does not require
    a real cluster for testing.
    
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>

---
+++ pkg/e2e/e2e.go
@@ -0,0 +1,51 @@
+// SPDX-FileCopyrightText: The RamenDR authors
+// SPDX-License-Identifier: Apache-2.0
+
+package e2e
+
+import (
+	"github.com/ramendr/ramen/e2e/dractions"
+	"github.com/ramendr/ramen/e2e/types"
+	"github.com/ramendr/ramen/e2e/util"
+	"github.com/ramendr/ramen/e2e/validate"
+)
+
+type Backend struct{}
+
+var _ Testing = &Backend{}
+
+func (Backend) Validate(ctx types.Context) error {
+	return validate.TestConfig(ctx)
+}
+
+func (Backend) Setup(ctx types.Context) error {
+	return util.EnsureChannel(ctx)
+}
+
+func (Backend) Cleanup(ctx types.Context) error {
+	return util.EnsureChannelDeleted(ctx)
+}
+
+func (Backend) Deploy(ctx types.TestContext) error {
+	return ctx.Deployer().Deploy(ctx)
+}
+
+func (Backend) Undeploy(ctx types.TestContext) error {
+	return ctx.Deployer().Undeploy(ctx)
+}
+
+func (Backend) Protect(ctx types.TestContext) error {
+	return dractions.EnableProtection(ctx)
+}
+
+func (Backend) Unprotect(ctx types.TestContext) error {
+	return dractions.DisableProtection(ctx)
+}
+
+func (Backend) Failover(ctx types.TestContext) error {
+	return dractions.Failover(ctx)
+}
+
+func (Backend) Relocate(ctx types.TestContext) error {
+	return dractions.Relocate(ctx)
+}
---
+++ pkg/e2e/testing.go
@@ -0,0 +1,22 @@
+// SPDX-FileCopyrightText: The RamenDR authors
+// SPDX-License-Identifier: Apache-2.0
+
+package e2e
+
+import "github.com/ramendr/ramen/e2e/types"
+
+// Testing interface for ramenctl commands.
+type Testing interface {
+	// Operations on types.Context.
+	Validate(types.Context) error
+	Setup(types.Context) error
+	Cleanup(types.Context) error
+
+	// Operations on types.TestContext.
+	Deploy(types.TestContext) error
+	Undeploy(types.TestContext) error
+	Protect(types.TestContext) error
+	Unprotect(types.TestContext) error
+	Failover(types.TestContext) error
+	Relocate(types.TestContext) error
+}
