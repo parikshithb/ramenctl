b71402427ba231ee2af3eec4594c5b3e2cb02754
Nir Soffer <nsoffer@redhat.com>
2025-04-29 00:53:45 +0300

    test: Add fast command tests
    Add command_test.go for fast command unit tests. We use fake config and
    env with the minimal configuration to make the test run without real
    clusters.
    
    With this change we test all possible positive and negative flows with
    all workloads, deployers and pvcSpecs in 59 milliseconds.
    
    Coverage for the test package increased from 16.1% to 75.2%.
    
    New tests:
    
        % go test -list='Test(Run|Clean)' ./pkg/test
        TestRun
        TestRunValidateFailed
        TestRunValidateCanceled
        TestRunSetupFailed
        TestRunSetupCanceled
        TestRunTestsFailed
        TestRunDisappFailed
        TestRunTestsCanceled
        TestClean
        TestCleanValidateFailed
        TestCleanValidateCanceled
        TestCleanTestsFailed
        TestCleanTestsCanceled
        TestCleanCleanupFailed
        TestCleanCleanupCanceled
    
    Example test:
    
        % go test -v -run TestRunDisappFailed ./pkg/test
        === RUN   TestRunDisappFailed
    
        🔎 Validate config ...
           ✅ Config validated
    
        🔎 Setup environment ...
           ✅ Environment setup
    
        🔎 Run tests ...
           ✅ Application "disapp-deploy-file" deployed
           ✅ Application "appset-deploy-block" deployed
           ✅ Application "appset-deploy-block" protected
           ✅ Application "disapp-deploy-file" protected
           ❌ Failed to failover application "disapp-deploy-file"
           ✅ Application "subscr-deploy-file" deployed
           ✅ Application "appset-deploy-file" deployed
           ✅ Application "disapp-deploy-block" deployed
           ✅ Application "disapp-deploy-block" protected
           ✅ Application "subscr-deploy-block" deployed
           ✅ Application "subscr-deploy-block" protected
           ✅ Application "subscr-deploy-block" failed over
           ✅ Application "subscr-deploy-block" relocated
           ❌ Failed to failover application "disapp-deploy-block"
           ✅ Application "appset-deploy-block" failed over
           ✅ Application "subscr-deploy-file" protected
           ✅ Application "appset-deploy-file" protected
           ✅ Application "subscr-deploy-file" failed over
           ✅ Application "subscr-deploy-block" unprotected
           ✅ Application "appset-deploy-block" relocated
           ✅ Application "appset-deploy-block" unprotected
           ✅ Application "appset-deploy-block" undeployed
           ✅ Application "appset-deploy-file" failed over
           ✅ Application "appset-deploy-file" relocated
           ✅ Application "subscr-deploy-file" relocated
           ✅ Application "subscr-deploy-file" unprotected
           ✅ Application "subscr-deploy-file" undeployed
           ✅ Application "subscr-deploy-block" undeployed
           ✅ Application "appset-deploy-file" unprotected
           ✅ Application "appset-deploy-file" undeployed
        --- PASS: TestRunDisappFailed (0.01s)
        PASS
        ok  	github.com/ramendr/ramenctl/pkg/test	0.417s
    
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>

---
+++ pkg/test/command_test.go
@@ -0,0 +1,404 @@
+// SPDX-FileCopyrightText: The RamenDR authors
+// SPDX-License-Identifier: Apache-2.0
+
+package test
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/ramendr/ramen/e2e/types"
+	"github.com/ramendr/ramenctl/pkg/command"
+)
+
+var testConfig = types.Config{
+	PVCSpecs: []types.PVCSpecConfig{
+		{Name: "block", StorageClassName: "block-storage"},
+		{Name: "file", StorageClassName: "file-storage"},
+	},
+	Tests: []types.TestConfig{
+		{Workload: "deploy", Deployer: "appset", PVCSpec: "block"},
+		{Workload: "deploy", Deployer: "appset", PVCSpec: "file"},
+		{Workload: "deploy", Deployer: "subscr", PVCSpec: "block"},
+		{Workload: "deploy", Deployer: "subscr", PVCSpec: "file"},
+		{Workload: "deploy", Deployer: "disapp", PVCSpec: "block"},
+		{Workload: "deploy", Deployer: "disapp", PVCSpec: "file"},
+	},
+}
+
+var testEnv = types.Env{
+	Hub: types.Cluster{Name: "hub"},
+	C1:  types.Cluster{Name: "c1"},
+	C2:  types.Cluster{Name: "c2"},
+}
+
+var testOptions Options
+
+var validateFailed = MockBackend{
+	ValidateFunc: func(ctx types.Context) error {
+		return errors.New("No validate for you!")
+	},
+}
+
+var validateCanceled = MockBackend{
+	ValidateFunc: func(ctx types.Context) error {
+		return context.Canceled
+	},
+}
+
+var setupFailed = MockBackend{
+	SetupFunc: func(ctx types.Context) error {
+		return errors.New("No setup for you!")
+	},
+}
+var setupCanceled = MockBackend{
+	SetupFunc: func(ctx types.Context) error {
+		return context.Canceled
+	},
+}
+
+var cleanupFailed = MockBackend{
+	CleanupFunc: func(ctx types.Context) error {
+		return errors.New("No cleanup for you!")
+	},
+}
+
+var cleanupCanceled = MockBackend{
+	CleanupFunc: func(ctx types.Context) error {
+		return context.Canceled
+	},
+}
+
+var failoverFailed = MockBackend{
+	FailoverFunc: func(ctx types.TestContext) error {
+		return errors.New("No failover for you!")
+	},
+}
+
+var failoverCanceled = MockBackend{
+	FailoverFunc: func(ctx types.TestContext) error {
+		return context.Canceled
+	},
+}
+
+var disappFailoverFailed = MockBackend{
+	FailoverFunc: func(ctx types.TestContext) error {
+		if ctx.Deployer().IsDiscovered() {
+			return errors.New("No failover for you!")
+		}
+		return nil
+	},
+}
+
+var unprotectFailed = MockBackend{
+	UnprotectFunc: func(ctx types.TestContext) error {
+		return errors.New("No unprotect for you!")
+	},
+}
+
+var unprotectCanceled = MockBackend{
+	UnprotectFunc: func(ctx types.TestContext) error {
+		return context.Canceled
+	},
+}
+
+func TestRun(t *testing.T) {
+	outputDir := t.TempDir()
+	cmd, err := command.ForTest("test-run", &testConfig, &testEnv, outputDir)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer cmd.Close()
+	test := newCommand(cmd, &MockBackend{}, testOptions)
+	if err := test.Run(); err != nil {
+		t.Fatal(err)
+	}
+	if test.Report.Status != Passed {
+		t.Errorf("expected status %q, got %q", Passed, test.Report.Status)
+	}
+	summary := Summary{Passed: len(testConfig.Tests)}
+	if test.Report.Summary != summary {
+		t.Errorf("expected summary %+v, got %+v", summary, test.Report.Summary)
+	}
+}
+
+func TestRunValidateFailed(t *testing.T) {
+	outputDir := t.TempDir()
+	cmd, err := command.ForTest("test-run", &testConfig, &testEnv, outputDir)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer cmd.Close()
+	test := newCommand(cmd, &validateFailed, testOptions)
+	if err := test.Run(); err == nil {
+		t.Fatal("command did not fail")
+	}
+	if test.Report.Status != Failed {
+		t.Errorf("expected status %q, got %q", Failed, test.Report.Status)
+	}
+	summary := Summary{}
+	if test.Report.Summary != summary {
+		t.Errorf("expected summary %+v, got %+v", summary, test.Report.Summary)
+	}
+}
+
+func TestRunValidateCanceled(t *testing.T) {
+	outputDir := t.TempDir()
+	cmd, err := command.ForTest("test-run", &testConfig, &testEnv, outputDir)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer cmd.Close()
+	test := newCommand(cmd, &validateCanceled, testOptions)
+	if err := test.Run(); err == nil {
+		t.Fatal("command did not fail")
+	}
+	if test.Report.Status != Canceled {
+		t.Errorf("expected status %q, got %q", Failed, test.Report.Status)
+	}
+	summary := Summary{}
+	if test.Report.Summary != summary {
+		t.Errorf("expected summary %+v, got %+v", summary, test.Report.Summary)
+	}
+}
+
+func TestRunSetupFailed(t *testing.T) {
+	outputDir := t.TempDir()
+	cmd, err := command.ForTest("test-run", &testConfig, &testEnv, outputDir)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer cmd.Close()
+	test := newCommand(cmd, &setupFailed, testOptions)
+	if err := test.Run(); err == nil {
+		t.Fatal("command did not fail")
+	}
+	if test.Report.Status != Failed {
+		t.Errorf("expected status %q, got %q", Failed, test.Report.Status)
+	}
+	summary := Summary{}
+	if test.Report.Summary != summary {
+		t.Errorf("expected summary %+v, got %+v", summary, test.Report.Summary)
+	}
+}
+
+func TestRunSetupCanceled(t *testing.T) {
+	outputDir := t.TempDir()
+	cmd, err := command.ForTest("test-run", &testConfig, &testEnv, outputDir)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer cmd.Close()
+	test := newCommand(cmd, &setupCanceled, testOptions)
+	if err := test.Run(); err == nil {
+		t.Fatal("command did not fail")
+	}
+	if test.Report.Status != Canceled {
+		t.Errorf("expected status %q, got %q", Canceled, test.Report.Status)
+	}
+	summary := Summary{}
+	if test.Report.Summary != summary {
+		t.Errorf("expected summary %+v, got %+v", summary, test.Report.Summary)
+	}
+}
+
+func TestRunTestsFailed(t *testing.T) {
+	outputDir := t.TempDir()
+	cmd, err := command.ForTest("test-run", &testConfig, &testEnv, outputDir)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer cmd.Close()
+	test := newCommand(cmd, &failoverFailed, testOptions)
+	if err := test.Run(); err == nil {
+		t.Fatal("command did not fail")
+	}
+	if test.Report.Status != Failed {
+		t.Errorf("expected status %q, got %q", Failed, test.Report.Status)
+	}
+	summary := Summary{Failed: len(testConfig.Tests)}
+	if test.Report.Summary != summary {
+		t.Errorf("expected summary %+v, got %+v", summary, test.Report.Summary)
+	}
+}
+
+func TestRunDisappFailed(t *testing.T) {
+	outputDir := t.TempDir()
+	cmd, err := command.ForTest("test-run", &testConfig, &testEnv, outputDir)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer cmd.Close()
+	test := newCommand(cmd, &disappFailoverFailed, testOptions)
+	if err := test.Run(); err == nil {
+		t.Fatal("command did not fail")
+	}
+	if test.Report.Status != Failed {
+		t.Errorf("expected status %q, got %q", Failed, test.Report.Status)
+	}
+	summary := Summary{Passed: 4, Failed: 2}
+	if test.Report.Summary != summary {
+		t.Errorf("expected summary %+v, got %+v", summary, test.Report.Summary)
+	}
+}
+
+func TestRunTestsCanceled(t *testing.T) {
+	outputDir := t.TempDir()
+	cmd, err := command.ForTest("test-run", &testConfig, &testEnv, outputDir)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer cmd.Close()
+	test := newCommand(cmd, &failoverCanceled, testOptions)
+	if err := test.Run(); err == nil {
+		t.Fatal("command did not fail")
+	}
+	if test.Report.Status != Canceled {
+		t.Errorf("expected status %q, got %q", Failed, test.Report.Status)
+	}
+	summary := Summary{Canceled: len(testConfig.Tests)}
+	if test.Report.Summary != summary {
+		t.Errorf("expected summary %+v, got %+v", summary, test.Report.Summary)
+	}
+}
+
+func TestClean(t *testing.T) {
+	outputDir := t.TempDir()
+	cmd, err := command.ForTest("test-run", &testConfig, &testEnv, outputDir)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer cmd.Close()
+	test := newCommand(cmd, &MockBackend{}, testOptions)
+	if err := test.Clean(); err != nil {
+		t.Fatal(err)
+	}
+	if test.Report.Status != Passed {
+		t.Errorf("expected status %q, got %q", Passed, test.Report.Status)
+	}
+	summary := Summary{Passed: 6}
+	if test.Report.Summary != summary {
+		t.Errorf("expected summary %+v, got %+v", summary, test.Report.Summary)
+	}
+}
+
+func TestCleanValidateFailed(t *testing.T) {
+	outputDir := t.TempDir()
+	cmd, err := command.ForTest("test-run", &testConfig, &testEnv, outputDir)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer cmd.Close()
+	test := newCommand(cmd, &validateFailed, testOptions)
+	if err := test.Clean(); err == nil {
+		t.Fatal("command did not fail")
+	}
+	if test.Report.Status != Failed {
+		t.Errorf("expected status %q, got %q", Failed, test.Report.Status)
+	}
+	summary := Summary{}
+	if test.Report.Summary != summary {
+		t.Errorf("expected summary %+v, got %+v", summary, test.Report.Summary)
+	}
+}
+
+func TestCleanValidateCanceled(t *testing.T) {
+	outputDir := t.TempDir()
+	cmd, err := command.ForTest("test-run", &testConfig, &testEnv, outputDir)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer cmd.Close()
+	test := newCommand(cmd, &validateCanceled, testOptions)
+	if err := test.Clean(); err == nil {
+		t.Fatal("command did not fail")
+	}
+	if test.Report.Status != Canceled {
+		t.Errorf("expected status %q, got %q", Canceled, test.Report.Status)
+	}
+	summary := Summary{}
+	if test.Report.Summary != summary {
+		t.Errorf("expected summary %+v, got %+v", summary, test.Report.Summary)
+	}
+}
+
+func TestCleanTestsFailed(t *testing.T) {
+	outputDir := t.TempDir()
+	cmd, err := command.ForTest("test-run", &testConfig, &testEnv, outputDir)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer cmd.Close()
+	test := newCommand(cmd, &unprotectFailed, testOptions)
+	if err := test.Clean(); err == nil {
+		t.Fatal("command did not fail")
+	}
+	if test.Report.Status != Failed {
+		t.Errorf("expected status %q, got %q", Failed, test.Report.Status)
+	}
+	summary := Summary{Failed: len(testConfig.Tests)}
+	if test.Report.Summary != summary {
+		t.Errorf("expected summary %+v, got %+v", summary, test.Report.Summary)
+	}
+}
+
+func TestCleanTestsCanceled(t *testing.T) {
+	outputDir := t.TempDir()
+	cmd, err := command.ForTest("test-run", &testConfig, &testEnv, outputDir)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer cmd.Close()
+	test := newCommand(cmd, &unprotectCanceled, testOptions)
+	if err := test.Clean(); err == nil {
+		t.Fatal("command did not fail")
+	}
+	if test.Report.Status != Canceled {
+		t.Errorf("expected status %q, got %q", Failed, test.Report.Status)
+	}
+	summary := Summary{Canceled: len(testConfig.Tests)}
+	if test.Report.Summary != summary {
+		t.Errorf("expected summary %+v, got %+v", summary, test.Report.Summary)
+	}
+}
+
+func TestCleanCleanupFailed(t *testing.T) {
+	outputDir := t.TempDir()
+	cmd, err := command.ForTest("test-run", &testConfig, &testEnv, outputDir)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer cmd.Close()
+	test := newCommand(cmd, &cleanupFailed, testOptions)
+	if err := test.Clean(); err == nil {
+		t.Fatal("command did not fail")
+	}
+	if test.Report.Status != Failed {
+		t.Errorf("expected status %q, got %q", Failed, test.Report.Status)
+	}
+	summary := Summary{Passed: len(testConfig.Tests)}
+	if test.Report.Summary != summary {
+		t.Errorf("expected summary %+v, got %+v", summary, test.Report.Summary)
+	}
+}
+
+func TestCleanCleanupCanceled(t *testing.T) {
+	outputDir := t.TempDir()
+	cmd, err := command.ForTest("test-run", &testConfig, &testEnv, outputDir)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer cmd.Close()
+	test := newCommand(cmd, &cleanupCanceled, testOptions)
+	if err := test.Clean(); err == nil {
+		t.Fatal("command did not fail")
+	}
+	if test.Report.Status != Canceled {
+		t.Errorf("expected status %q, got %q", Failed, test.Report.Status)
+	}
+	summary := Summary{Passed: len(testConfig.Tests)}
+	if test.Report.Summary != summary {
+		t.Errorf("expected summary %+v, got %+v", summary, test.Report.Summary)
+	}
+}
