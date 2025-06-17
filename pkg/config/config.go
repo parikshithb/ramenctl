594b3dd0e78c5498f78559e9041d5c2c0456023a
Nir Soffer <nsoffer@redhat.com>
2025-06-17 14:29:33 +0300

    config: Introduce generic config
    Add config.Config type and config.ReadConfig() for reading it. This will
    be used by the generic command to read the configuration file.
    
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -6,11 +6,34 @@ package config
 import (
 	"errors"
 	"fmt"
+	"maps"
 	"os"
 
+	"github.com/spf13/viper"
+
+	"github.com/ramendr/ramen/e2e/config"
 	"github.com/ramendr/ramenctl/pkg/console"
 )
 
+// Config is used for all ramenctl commands except the test commands. It is a subset of
+// ramen/e2e/config.Config.
+type Config struct {
+	// Clusters are part of this environment. Requires "hub", "c1", and "c2".
+	Clusters map[string]config.Cluster `json:"clusters"`
+
+	// ClusterSet name with the managed clusters.
+	ClusterSet string `json:"clusterSet"`
+
+	// Distro is either config.DistroK8s or config.DistroOcp. If unset the distro is detected
+	// automatically when validating the config with the clusters.
+	Distro string `json:"distro"`
+
+	// Namespaces are set automatically based on Distro.
+	Namespaces config.Namespaces `json:"namespaces"`
+}
+
+// CreateSampleConfig create a sample config that can be used by all commands. The file can be
+// parsed using ReadConfig() or test.readConfig().
 func CreateSampleConfig(filename, commandName, envFile string) error {
 	var sample *Sample
 	if envFile != "" {
@@ -34,6 +57,79 @@ func CreateSampleConfig(filename, commandName, envFile string) error {
 			return fmt.Errorf("configuration file %q already exists", filename)
 		}
 		return fmt.Errorf("failed to create %q: %w", filename, err)
+	}
+	return nil
+}
+
+// ReadConfig reads the configuration file created by CreateSampleConfig, ignoring the test only
+// configuration.
+func ReadConfig(filename string) (*Config, error) {
+	viper.SetDefault("ClusterSet", config.DefaultClusterSetName)
+	viper.SetConfigFile(filename)
+
+	if err := viper.ReadInConfig(); err != nil {
+		return nil, fmt.Errorf("failed to read config: %v", err)
+	}
+
+	cfg := &Config{}
+	if err := viper.Unmarshal(cfg); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal config: %v", err)
+	}
+
+	if err := cfg.validateDistro(); err != nil {
+		return nil, err
+	}
+
+	if err := cfg.validateClusters(); err != nil {
+		return nil, err
+	}
+
+	return cfg, nil
+}
+
+// Equal return true if config is equal to other config.
+func (c *Config) Equal(o *Config) bool {
+	if c == o {
+		return true
+	}
+	if c.Distro != o.Distro {
+		return false
+	}
+	if c.ClusterSet != o.ClusterSet {
+		return false
+	}
+	if c.Namespaces != o.Namespaces {
+		return false
+	}
+	return maps.Equal(c.Clusters, o.Clusters)
+}
+
+func (c *Config) validateDistro() error {
+	if c.Distro == "" {
+		// Will be detected when accessing the clusters.
+		return nil
+	}
+	switch c.Distro {
+	case config.DistroK8s:
+		c.Namespaces = config.K8sNamespaces
+	case config.DistroOcp:
+		c.Namespaces = config.OcpNamespaces
+	default:
+		return fmt.Errorf("invalid distro %q: (choose one of %q, %q)",
+			c.Distro, config.DistroK8s, config.DistroOcp)
+	}
+	return nil
+}
+
+func (c *Config) validateClusters() error {
+	if c.Clusters["hub"].Kubeconfig == "" {
+		return fmt.Errorf("failed to find hub cluster in configuration")
+	}
+	if c.Clusters["c1"].Kubeconfig == "" {
+		return fmt.Errorf("failed to find c1 cluster in configuration")
+	}
+	if c.Clusters["c2"].Kubeconfig == "" {
+		return fmt.Errorf("failed to find c2 cluster in configuration")
 	}
 	return nil
 }
--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -7,6 +7,8 @@ import (
 	"reflect"
 	"testing"
 
+	e2econfig "github.com/ramendr/ramen/e2e/config"
+
 	"github.com/ramendr/ramenctl/pkg/config"
 )
 
@@ -60,4 +62,83 @@ func TestSampleFromEnv(t *testing.T) {
 	if !reflect.DeepEqual(expected, sample) {
 		t.Fatalf("expected %+v, got %+v", expected, sample)
 	}
+}
+
+func testConfig() *config.Config {
+	return &config.Config{
+		Clusters: map[string]e2econfig.Cluster{
+			"hub": {Kubeconfig: "hub/config"},
+			"c1":  {Kubeconfig: "dr1/config"},
+			"c2":  {Kubeconfig: "dr2/config"},
+		},
+		ClusterSet: "default",
+	}
+}
+
+func TestReadConfigGeneric(t *testing.T) {
+	// We read the same config from full test config or the simplified test config.
+	c, err := config.ReadConfig("testdata/generic.yaml")
+	if err != nil {
+		t.Fatal(err)
+	}
+	expected := testConfig()
+	if !c.Equal(expected) {
+		t.Fatalf("expected %+v, got %+v", expected, c)
+	}
+}
+
+func TestReadConfigTest(t *testing.T) {
+	// We read the same config from full test config or the simplified test config.
+	c1, err := config.ReadConfig("testdata/test.yaml")
+	if err != nil {
+		t.Fatal(err)
+	}
+	c2, err := config.ReadConfig("testdata/generic.yaml")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !c1.Equal(c2) {
+		t.Fatalf("expected %+v, got %+v", c1, c2)
+	}
+}
+
+func TestConfigEqual(t *testing.T) {
+	c1 := testConfig()
+	t.Run("equal to itself", func(t *testing.T) {
+		c2 := c1
+		if !c1.Equal(c2) {
+			t.Errorf("config %+v is not equal to itself", c1)
+		}
+	})
+	t.Run("equal to other identical config", func(t *testing.T) {
+		c2 := testConfig()
+		if !c1.Equal(c2) {
+			t.Errorf("config %+v is not equal to other identical config %+v", c1, c2)
+		}
+	})
+}
+
+func TestConfigNotEqual(t *testing.T) {
+	c1 := testConfig()
+	t.Run("distro", func(t *testing.T) {
+		c2 := testConfig()
+		c2.Distro = "modified"
+		if c1.Equal(c2) {
+			t.Fatalf("config %+v non equal config %+v", c1, c2)
+		}
+	})
+	t.Run("clusterset", func(t *testing.T) {
+		c2 := testConfig()
+		c2.ClusterSet = "modified"
+		if c1.Equal(c2) {
+			t.Fatalf("config %+v non equal config %+v", c1, c2)
+		}
+	})
+	t.Run("clusters", func(t *testing.T) {
+		c2 := testConfig()
+		c2.Clusters["c2"] = e2econfig.Cluster{Kubeconfig: "modified"}
+		if c1.Equal(c2) {
+			t.Fatalf("config %+v non equal config %+v", c1, c2)
+		}
+	})
 }
