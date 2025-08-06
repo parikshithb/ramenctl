cbe64ac637f0a04d4de8017e25786cb8f31f3a03
Nir Soffer <nsoffer@redhat.com>
2025-08-07 01:02:52 +0300

    report: Experiment with emoji
    Add emoji to condition status to make it possible to understand the
    report in a quick look without reading the content.
    
    This is not very friendly for a machine, but Go code can check easily
    compare the constants.
    
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>

--- pkg/report/conditions.go
+++ pkg/report/conditions.go
@@ -7,11 +7,11 @@ type ConditionStatus string
 
 const (
 	// Condition status is expected.
-	ConditionOK = ConditionStatus("ok")
+	ConditionOK = ConditionStatus("ok ✅")
 
 	// Condition generation does not match object generation.
-	ConditionStale = ConditionStatus("stale")
+	ConditionStale = ConditionStatus("stale 🟠")
 
 	// Condition status is not expected.
-	ConditionError = ConditionStatus("error")
+	ConditionError = ConditionStatus("error ❌")
 )
